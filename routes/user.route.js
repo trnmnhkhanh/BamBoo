96fd2559d511863f012de8473195fb65d56eb82c
Coders.Mickey <thuongnguyen.it78@gmail.com>
2020-08-05 15:21:58 +0700

    set up route user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
-
-
-
-
 const controller = require('../controller/user.controller');
+
 const validate = require('../validate/user.validate')
+
 const authMiddleware = require('../middlewares/auth.middleware');
 
 var router = express.Router();
@@ -13,16 +11,7 @@ var router = express.Router();
 
 
 
-router.get('/', authMiddleware.requireAuth, controller.index);
-
-
-router.get('/search', controller.search);
-
-router.get('/create', controller.create);
-
-router.get('/:id', controller.get)
-
-router.post('/create',validate.postCreate, controller.postCreate);
-
+router.get('/', controller.index); // authMiddleware.requireAuth
+router.get('/signout', controller.signout);
 
 module.exports = router;
