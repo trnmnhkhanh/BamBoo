19c2d234347284eb33e020d9a9202e6301a98313
trnmnhkhanh <49711828+trnmnhkhanh@users.noreply.github.com>
2020-08-12 21:06:30 +0700

    Update auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,9 +7,9 @@ var router = express.Router();
 router.get('/login', controller.login);
 
 router.post('/login', controller.postLogin)
-
+// 1.1 gọi router để kết nối controller với view
 router.get('/signup', controller.signup)
-
+// 3.1 gọi router để kết nối controller với view
 router.post('/signup', controller.postSignup)
 
 router.get('/forgetpw', controller.forgetPw);
